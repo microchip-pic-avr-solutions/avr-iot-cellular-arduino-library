86c91e92f8476a91631438d8b71c266a7621e246
Simen Gangstad <simen.gangstad@microchip.com>
2021-07-07 09:48:13 +0200

    Include compilation of cryptoauthlib

--- make.sh
+++ make.sh
@@ -17,12 +17,6 @@ if grep -q microsoft /proc/version; then
     CRYPTOAUTH_SRC_DIRPATH=$(wslpath -wa $CRYPTOAUTH_SRC_DIRPATH)
 fi
 
-if [ "$1" = "flash" ]; then
-    EXTRA_ARGS=-u
-elif [ "$1" = "clean" ]; then
-    EXTRA_ARGS=--clean
-fi
-
 pushd $CRYPTOAUTH_PATH
     mkdir build
     pushd build
@@ -31,12 +25,19 @@ pushd $CRYPTOAUTH_PATH
     popd
 popd
 
-arduino-cli.exe compile $TARGET \
-                        $EXTRA_ARGS \
-                        -t \
-                        -p $PORT \
-                        -P nedbg \
-                        -b DxCore:megaavr:avrdb:appspm=no,chip=128DB64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8  \
-                        --build-property "compiler.c.extra_flags=-I$CRYPTOAUTH_SRC_DIRPATH -I$CRYPTOAUTH_LIB_DIRPATH" \
-                        --build-property "compiler.cpp.extra_flags=-I$CRYPTOAUTH_SRC_DIRPATH -I$CRYPTOAUTH_LIB_DIRPATH" \
-                        --build-property "combine.extra_paths=$CRYPTOAUTH_LIB_PATH"
+if [ "$1" = "flash" ]; then
+    EXTRA_ARGS=-u
+elif [ "$1" = "clean" ]; then
+    EXTRA_ARGS=--clean
+fi
+
+arduino-cli.exe compile \
+                $TARGET \
+                $EXTRA_ARGS \
+                -t \
+                -p $PORT \
+                -P nedbg \
+                -b DxCore:megaavr:avrdb:appspm=no,chip=128DB64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8 \
+                --build-property "compiler.c.extra_flags=-I$CRYPTOAUTH_SRC_DIRPATH -I$CRYPTOAUTH_LIB_DIRPATH" \
+                --build-property "compiler.cpp.extra_flags=-I$CRYPTOAUTH_SRC_DIRPATH -I$CRYPTOAUTH_LIB_DIRPATH" \
+                --build-property "combine.extra_paths=$CRYPTOAUTH_LIB_PATH"
