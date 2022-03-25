9f8cc65f6e2d245e8f575565e42ee26ea6823cc2
Simen Gangstad <simen.gangstad@microchip.com>
2022-03-25 09:17:57 +0100

    Update for mini

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -17,4 +17,4 @@ arduino-cli.exe upload \
                 -t \
                 -p $PORT \
                 -P nedbg \
-                -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 \
+                -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 \
--- scripts/make.sh
+++ scripts/make.sh
@@ -14,5 +14,5 @@ rm -r "$SCRIPTPATH/../build"
 arduino-cli compile \
                 "$TARGET" \
                 $EXTRA_ARGS \
-                -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 \
+                -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 \
                 --output-dir "$SCRIPTPATH/../build"
