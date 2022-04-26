970517479758a577fb3a61fb83ddfe3a7ca0e4ac
m19936 <johan.lofstad@microchip.com>
2022-04-26 11:04:18 +0200

    Updated readme and moved examples folder one level up

--- scripts/compile_examples.sh
+++ scripts/compile_examples.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Test for AVR-IoT Cellular
-for d in src/examples/*/ ; do
+for d in examples/*/ ; do
         echo "Compiling $d...";
         arduino-cli compile -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 --libraries=".." "$d";
 done
 
 # Test for AVR-IoT Cellular Mini
-for d in src/examples/*/ ; do
+for d in examples/*/ ; do
         echo "Compiling $d...";
         arduino-cli compile -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 --libraries=".." "$d" --output-dir "builds/mini/$(basename $d)";
 done
