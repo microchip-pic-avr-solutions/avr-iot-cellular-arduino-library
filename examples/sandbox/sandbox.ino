d71c25d03cba66a06b2fc3e2073755c817fa471f
m19936 <johan.lofstad@microchip.com>
2022-04-29 10:50:52 +0200

    Removed unused function

--- examples/sandbox/sandbox.ino
+++ examples/sandbox/sandbox.ino
@@ -219,13 +219,6 @@ void decodeMessage(const char *message) {
     }
 }
 
-void remove_spaces(char *s) {
-    char *d = s;
-    do {
-        while (*d == ' ') { ++d; }
-    } while (*s++ = *d++);
-}
-
 void printHelp() {
     Log.rawf("\nAvailable Commands\n"
              "-----------------------\n"
