478ee74e63751dd78b1dfef83c7ac4a9f137df0f
m19936 <johan.lofstad@microchip.com>
2022-07-06 14:28:51 +0200

    MCU8MASS-488: Added arduino-lint stage, removed cryptoauthlib stage

--- scripts/compile_examples.sh
+++ scripts/compile_examples.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Test for AVR-IoT Cellular
-for d in examples/*/ ; do
-        echo "Compiling $d...";
-        arduino-cli compile -b DxCore:megaavr:avrdb:appspm=no,chip=avr128db64,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2 --libraries=".." "$d";
-done
-
 # Test for AVR-IoT Cellular Mini
 for d in examples/*/ ; do
         echo "Compiling $d...";
