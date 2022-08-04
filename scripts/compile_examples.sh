8b71dd17732ffc70292003fc93f0486b2adb989d
Simen Gangstad <simen.gangstad@microchip.com>
2022-08-04 15:21:58 +0200

    Enable full printf support (with floats) as we use float printing in e.g. the supply voltage example

--- scripts/compile_examples.sh
+++ scripts/compile_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2"
+BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2,printf=full"
 
 # Test for AVR-IoT Cellular Mini
 for d in examples/*/ ; do
--- scripts/make.sh
+++ scripts/make.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 TARGET=$SCRIPTPATH/../examples/sandbox/sandbox.ino
-BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2"
+BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2,printf=full"
 
 if [ -d "$SCRIPTPATH/../build" ]; then
     rm -r "$SCRIPTPATH/../build"
