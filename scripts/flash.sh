6285388d92f7e9a4b143b0a9f6744cc192965ab6
Simen Gangstad <simen.gangstad@microchip.com>
2022-08-04 13:49:19 +0200

    Use pymcuprog for flashing, quite a lot faster

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-PORT=COM4
-BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2"
-BUILD_DIR=$SCRIPTPATH/../build
+TARGET=$(find $SCRIPTPATH/../build -type f -name "*.hex")
 
 echo "Flashing..."
 
 # Do path conversion for WSL
 if grep -q microsoft /proc/version; then
-    BUILD_DIR=$(wslpath -wa $BUILD_DIR)
-    arduino-cli.exe upload --input-dir $BUILD_DIR -t -p $PORT -P nedbg -b $BOARD_CONFIG
+    TARGET=$(wslpath -wa $TARGET)
+    pymcuprog.exe write --erase -d avr128db48 -f $TARGET
 else
-    arduino-cli upload --input-dir $BUILD_DIR -t -p $PORT -P nedbg -b $BOARD_CONFIG
+    pymcuprog write --erase -d avr128db48 -f $TARGET
 fi
 
