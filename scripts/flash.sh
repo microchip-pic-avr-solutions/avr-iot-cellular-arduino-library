590b5dc81ae0b15df6c04cc5f2358859d289d1f8
Simen Gangstad <simen.gangstad@microchip.com>
2022-08-05 15:17:10 +0200

    Add nEDBG as tool for flash script to differentiate from other boards

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -8,8 +8,8 @@ echo "Flashing..."
 # Do path conversion for WSL
 if grep -q microsoft /proc/version; then
     TARGET=$(wslpath -wa $TARGET)
-    pymcuprog.exe write --erase -d avr128db48 -f $TARGET
+    pymcuprog.exe write --erase -d avr128db48 -t nEDBG -f $TARGET
 else
-    pymcuprog write --erase -d avr128db48 -f $TARGET
+    pymcuprog write --erase -d avr128db48 -t nEDBG -f $TARGET
 fi
 
