72bd2c385e33d213fadf972aed7b53fb2fce39c2
Simen Gangstad <simen.gangstad@microchip.com>
2022-12-19 13:38:05 +0100

    MCU8MASS-976 MCU8MASS-942 MCU8MASS-1166 Update make script

--- scripts/make.bat
+++ scripts/make.bat
@@ -4,7 +4,7 @@ pushd %~dp0
 set SCRIPTPATH=%CD%
 popd
 
-set TARGET="%SCRIPTPATH%\..\examples\mqtt_provision\mqtt_provision.ino"
+set TARGET="%SCRIPTPATH%\..\examples\sanbox\sanbox.ino"
 set BUILD_DIR="%SCRIPTPATH%\..\build"
 set BOARD_CONFIG="DxCore:megaavr:avrdb:appspm=no,chip=avr128db48,clock=24internal,bodvoltage=1v9,bodmode=disabled,eesave=enable,resetpin=reset,millis=tcb2,startuptime=8,wiremode=mors2,printf=full"
 
